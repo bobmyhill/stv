81c7c6ae96ef7b8e3f3c22c97a92ee3e99e40d9d
Bob Myhill <myhill.bob@gmail.com>
2015-10-14 23:02:45 +0200

    Update

--- figures/plot_parameters.bash
+++ figures/plot_parameters.bash
@@ -1,8 +1,13 @@
 #!/bin/bash
+gmtset FONT_ANNOT_PRIMARY	   = 10p,4,black
+gmtset FONT_LABEL        	   = 14p,4,black
 
 base="ambient_stv_cell_parameters"
 
-psbasemap -JX10 -R0/1/2.5/5 -Ba0.2f0.1:"x Al":/a0.5f0.1:"Cell length (\305)":SWn -K -P > ${base}.ps
+
+psbasemap -JX10 -R0/1/40/70 -Ba0.2f0.1:"x Al":/a5f1:"Cell volume (\305@+3@+)":E  -K -P > ${base}.ps
+psbasemap -JX10 -R0/1/2.5/5 -Ba0.2f0.1:"1 - X@-SiO2@-":/a0.5f0.1:"Cell length (\305)":SWn -O -K -P >> ${base}.ps
+
 awk 'NR>1 {print $1, $2, $2-$3, $2, $2, $2+$3}' volumes.dat | psxy -J -R -O -K -N -Sc0.1c -Gred -EY0 >> ${base}.ps
 awk 'NR>1 {print $1, $4, $4-$5, $4, $4, $4+$5}' volumes.dat | psxy -J -R -O -K -N -Sc0.1c -Gred -EY0 >> ${base}.ps
 awk 'NR>1 {print $1, $2}' volumes.dat | psxy -J -R -O -K -N -W1,red,. >> ${base}.ps
@@ -10,8 +15,7 @@ awk 'NR>1 {print $1, $4}' volumes.dat | psxy -J -R -O -K -N -W1,red,. >> ${base}
 awk 'NR>1 {print $1, $6, $6-$7, $6, $6, $6+$7}' volumes.dat | psxy -J -R -O -K -N -Sc0.1c -Gblue -EY0 >> ${base}.ps
 
 
-psbasemap -JX10 -R0/1/40/70 -Ba0.2f0.1:"x Al":/a5f1:"Cell volume (\305@+3@+)":E -O -K >> ${base}.ps
-awk 'NR>1 {print $1, ($2*$4*$6)}' volumes.dat | psxy -J -R -O -K -N -Sc0.1c -Gblack  >> ${base}.ps
+awk 'NR>1 {print $1, ($2*$4*$6)}' volumes.dat | psxy -JX10 -R0/1/40/70 -O -K -N -Sc0.1c -Gblack  >> ${base}.ps
 
 awk 'NR>1 {print $1, $2, $2-$3, $2, $2, $2+$3}' volumes_2.dat | psxy -J -R -O -K -N -EY0 -Sc0.1c -Gblack  >> ${base}.ps
 awk 'NR>1 {print $1, ($2*$4*$6)}' volumes.dat | awk '$1<0.001 || $1 > 0.999' | psxy -J -R -O -N -W1,black,- >> ${base}.ps
